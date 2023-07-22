d33d003b8dc09e858e49780f26bebeaf5182fb11
BebopingBooberies <140271279+BebopingBooberies@users.noreply.github.com>
2023-07-22 22:04:42 +0100

    Add files via upload

---
+++ html/WeatherScript.js
@@ -0,0 +1,22 @@
+const getTemp = document.getElementById('weather')
+const setTemp = document.getElementById("today")
+
+
+getWeatherData()
+function getWeatherData () {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=Fordbridge&units=metric&appid=5d3f345a0c084277fecfd6453ad0efce').then(res => res.json()).then(data => {
+
+        console.log(data)
+        showWeatherData(data);
+    })
+
+}
+
+function showWeatherData (data) {
+    getTemp.window.onload =
+        '<img src="http://openweatherapp.org/img/wn.${day.weather[0].icon}@4x.png" alt="weather icon" className="w-icon">\n' +
+        ' <div className="temp">Current - ${day.main.temp}; C</div>\n' +
+        ' <div className="temp_max">High - ${day.main.temp_max}; C</div>\n' +
+        ' <div className="temp_min">Low - ${day.main.temp_min}</div>\n';
+}
+
