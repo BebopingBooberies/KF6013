5db9a807b84d09657e92344aa87ce1b1cb651816
BebopingBooberies <kerushichan121@gmail.com>
2023-08-03 00:08:19 +0100

    weather script and Google Maps script working and sorted

--- html/GoogleMaps.js
+++ html/GoogleMaps.js
@@ -1,26 +1,15 @@
 
-async function initMap() {
-    let latlng = {lat: 52.4854415, lng: -1.7472627};
-    let map = new google.maps.Map(
+function initMap() {
+    const latlng = {lat: 52.4854415, lng: -1.7472627};
+    const map = new google.maps.Map(
         document.getElementById('map'), {
             zoom: 14,
-            center: latlng
+            center: latlng,
         }
     );
 
-    let marker = new google.maps.Marker({position: latlng, map: map});
-
-    /*let mapOptions = {
-        key: AIzaSyDp_G9IHMvZPa34DVhY1kzvWOfojEMF4zo,
-        center:new google.maps.LatLng(52.482914, -1.744123),
-        zoom: 14,
-        mapTypeID: google.maps.MapTypeId.SATELLITE,
-        streetViewControl: false,
-        overviewMapControl: false,
-        rotateControl: false,
-        scaleControl: false,
-        panControl: false,
-    };
-
-    map = new google.maps.Map(document.getElementById("map"), mapOptions) ;*/
-}
+    new google.maps.Marker({
+        position: latlng,
+        map
+    })
+};
--- html/WeatherScript.js
+++ html/WeatherScript.js
@@ -1,21 +1,22 @@
-const getTemp = document.getElementById('weather');
-
+const getTemp = document.getElementById('today');
 
 getWeatherData()
-function getWeatherData () {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=Fordbridge&units=metric&appid=5d3f345a0c084277fecfd6453ad0efce').then(res => res.json()).then(data => {
-
-        console.log(data)
-        showWeatherData(data);
+function getWeatherData() {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=Fordbridge&units=metric&appid=5d3f345a0c084277fecfd6453ad0efce')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            showWeatherData(data);
     })
-
 }
 
-function showWeatherData (data) {
-    getTemp.window.onload =
-        '<img src="http://openweatherapp.org/img/wn.${day.weather[1].icon}@4x.png" alt="weather icon" className="w-icon">\n' +
-        ' <div className="temp">Current - ${day.main.temp}; C</div>\n' +
-        ' <div className="temp_max">High - ${day.main.temp_max}; C</div>\n' +
-        ' <div className="temp_min">Low - ${day.main.temp_min}</div>\n';
+function showWeatherData(data) {
+    getTemp.innerHTML =
+        `<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png" alt="weather icon" className="w-icon">
+        <div class="Weather-type">the weather forecast is: ${data.weather[0].description}</div>
+        <div className="temp">Current - ${data.main.temp}&#176;C</div>
+        <div className="temp_max">High - ${data.main.temp_max}&#176;C</div>
+        <div className="temp_min">Low - ${data.main.temp_min}&#176;C</div>`;
 }
 
+getWeatherData();
