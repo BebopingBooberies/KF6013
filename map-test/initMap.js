6694f3f2cfc9badde273b052dcc2db918ddebc22
BebopingBooberies <kerushichan121@gmail.com>
2023-08-07 21:46:59 +0100

    testing maps api

--- map-test/initMap.js
+++ map-test/initMap.js
@@ -1,16 +1,11 @@
-/**
- * @license
- * Copyright 2019 Google LLC. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
 let map;
 
 async function initMap() {
     const { Map } = await google.maps.importLibrary("maps");
 
     map = new Map(document.getElementById("map"), {
-        center: { lat: -35.397, lng: 150.644 },
-        zoom: 8,
+        center: { lat: 52.482914, lng: -1.744123 },
+        zoom: 14,
     });
 }
 
