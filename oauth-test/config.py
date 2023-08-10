77696fd6f121e9b0ad40043b4354f1e70f15581f
BebopingBooberies <kerushichan121@gmail.com>
2023-08-10 14:56:15 +0100

    Google OAuth to be implemented onto aws ec2 instance through GitHub.

---
+++ oauth-test/config.py
@@ -0,0 +1,13 @@
+# config.py
+
+config = {
+    "web": {
+        "client_id":"476907484548-26ejqjeab6cftc3gvjdgvub9i5bll0ka.apps.googleusercontent.com",
+        "project_id":"resit-assignment-project",
+        "auth_uri":"https://accounts.google.com/o/oauth2/auth",
+        "token_uri":"https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
+        "client_secret":"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",
+        "javascript_origins":["http://ec2test.kf6013-resit.com"]
+    }
+}
