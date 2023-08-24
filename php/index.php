a7a9facf16dab48010c688dc22820acfef135cd6
BebopingBooberies <kerushichan121@gmail.com>
2023-08-24 21:55:05 +0100

    php cannot be saved

--- php/index.php
+++ php/index.php
@@ -1,21 +1,15 @@
 <?php
 // author: Kelsey De Los Reyes Andrews
 //date: 16/03/2023
-//
 require_once __DIR__ . '/oauth-files/vendor/autoload.php';
 
 session_start() ;
-//create a new instance of google client
 $client = new Google\Client();
-//configure the client with the details provided in the Google OAuth client JSON file
 $client->setAuthConfigFile("/KF6013/oauth-files/client-secret.json");
 $client->addScope("https://www.googleapis.com/auth/userinfo.email");
 
-//check to see if there is an access token
 if (!empty($_SESSION['access_token'])) {
-    //set the Google OAuth client access token
     $client->setAccessToken($_SESSION["access_token"]);
-    //create some html to notify the user that they have been successfully authenticated to use this page
     $html = <<<HTML
     <!DOCTYPE html>
     <html lang=\"en\">
@@ -29,8 +23,8 @@ if (!empty($_SESSION['access_token'])) {
             <h1>Coast City Sports Centre</h1>
         </header>
         <nav>
-            <a href="index.html">Home</a>
-            <a href="Evaluation.html">Technical Architecture</a>
+            <a href="../html/index.html">Home</a>
+            <a href="../html/Evaluation.html">Technical Architecture</a>
             <a href="index.php">Members Page</a>
         </nav>
     
@@ -49,18 +43,16 @@ if (!empty($_SESSION['access_token'])) {
 
     echo $html;
 } else {
-    //if no access token exists redirect to the oauth2callback page which asks the user to sign in with their google
-    // credentials
+
     $redirect_uri = "http://" . $_SERVER["HTTP_HOST"] . "/KF6013/oauth2callback.php";
     header("Location: " . filter_var($redirect_uri, FILTER_SANITIZE_URL));
     exit();
 }
-//checks to see if the sign out button has been pressed
+
 if ($_SERVER["REQUEST_METHOD"] == "POST" and isset($_POST['log-out'])) {
     //revoke the client token so the user can't use the app unless they sign in again
     $client->revokeToken($_SESSION['access_token']);
 
-    //destroy the session
     session_destroy();
 
     //redirect to a different page (the home page of the website)
--- php/oauth2callback.php
+++ php/oauth2callback.php
@@ -1,26 +1,18 @@
 <?php
 // author: Kelsey De Los Reyes Andrews
 // date: 16/08/2023
-// run the autoload.php file that is in the vendor file and contains the google oauth dependancies
 require_once __DIR__.'/oauth-files/vendor/autoload.php';
 
 session_start();
-//create a new google client instance
 $client = new Google\Client();
-// configure the client with the details provided in the Google OAuth client JSON file
 $client->setAuthConfigFile('/KF6013/oauth-files/client-secret.json');
-// set the redirect url
 $client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . '/KF6013/oauth2callback.php');
-// add a scope for authorising the app with info on the google oauth
 $client->addScope("https://www.googleapis.com/auth/userinfo.email");
 
-// if the user isn't authorised to use the app
 if (!isset($_GET['code'])) {
-    //send the user to the google sign in page
     $auth_url = $client->createAuthUrl();
     header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
 } else {
-    // otherwise get the user sign in information and redirect them to the index.php page
     $client->authenticate($_GET['code']);
     $_SESSION['access_token'] = $client->getAccessToken();
     $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/KF6013/index.php';
